bf7d0df73b7a92d58c79982155ec70e9b49e3ea8
Tao Qian <tao@brightgo.com>
2024-09-01 09:42:31 -0700

    add chat

--- app/(tabs)/chat.tsx
+++ app/(tabs)/chat.tsx
@@ -1,13 +1,34 @@
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useState, useCallback } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { GiftedChat, IMessage } from 'react-native-gifted-chat';
 
 export default function ChatScreen() {
+  const [messages, setMessages] = useState<IMessage[]>([])
+
+  const onSend = useCallback(async (newMessages: IMessage[] = []) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, newMessages)
+    );
+  }, []);
+
   return (
-    <View>
-      <Text>hi</Text>
+    <View style={styles.container}>
+      <GiftedChat
+        messages={messages}
+        onSend={onSend}
+        user={{
+          _id: 1,
+          name: 'User',
+        }}
+        maxInputLength={500}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
 });
