c2c0d84b89bd89eacc59e735f61fd60c9384d301
Tao Qian <tao@brightgo.com>
2024-09-01 09:35:19 -0700

    add chat tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -32,6 +32,15 @@ export default function TabLayout() {
           ),
         }}
       />
+      <Tabs.Screen
+        name="chat"
+        options={{
+          title: 'Chat',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'chatbubbles' : 'chatbubbles-outline'} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
---
+++ app/(tabs)/chat.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function ChatScreen() {
+  return (
+    <View>
+      <Text>hi</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
